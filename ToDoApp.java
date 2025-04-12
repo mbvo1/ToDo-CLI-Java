d9c91ebfce9cff323d51fb6067c0e5ec7de4befd
Marcelo Bresani Victor De Oliveira <169096428+mbvo1@users.noreply.github.com>
2025-04-12 12:39:49 -0300

    Update ToDoApp.java

--- ToDoApp.java
+++ ToDoApp.java
@@ -7,9 +7,6 @@ public class ToDoApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcao;
-
-
-
         do {
             System.out.println("\n--- GERENCIADOR DE TAREFAS ---");
             System.out.println("1. Adicionar tarefa");
